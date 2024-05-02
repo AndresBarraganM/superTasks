bebe5c65d9fb5460a35667855aabaecda805031c
AndresBarraganM <al22760549@ite.edu.mx>
2024-05-02 09:25:49 -0700

    Creados los modelos para categoria y evento

--- app/src/main/java/com/example/supertasks/modelos/Categoria.kt
+++ app/src/main/java/com/example/supertasks/modelos/Categoria.kt
@@ -1,5 +1,63 @@
 package com.example.supertasks.modelos
+
+import java.util.Date
+
 //Clase que representa categorias
 
 class Categoria {
+    private var id_categoria: Int = 0 //ID de la bd
+        get()= field
+        set(value) {field = value }
+
+    private var nombre: String = "Nombre de evento" //nombre del evento
+        get() = field
+        set(value) {field = value}
+
+    private var descripcion: String? = "Descripcion de evento" //Descripcion del evento
+        get() = field
+        set(value) {field = value}
+
+    private var color: String? = null //Color de el evento, por ver si se implementara
+        get() = field
+        set(value) {field = value}
+
+    //Constructor
+        //Constructor completo
+    constructor(id_categoria: Int, nombre: String, descripcion: String?, color: String?) {
+        this.id_categoria = id_categoria
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.color = color
+    }
+
+
+        //Constructor con cosas necesarisas
+    constructor(id_categoria: Int, nombre: String) {
+        this.id_categoria = id_categoria
+        this.nombre = nombre
+    }
+
+    //to_string
+    override fun toString(): String {
+        return "Categoria(id_categoria=$id_categoria, nombre='$nombre', descripcion=$descripcion, color=$color)"
+    }
+
+    //compare
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Categoria
+
+        if (nombre != other.nombre) return false
+        if (descripcion != other.descripcion) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+
 }
--- app/src/main/java/com/example/supertasks/modelos/Evento.kt
+++ app/src/main/java/com/example/supertasks/modelos/Evento.kt
@@ -1,5 +1,88 @@
 package com.example.supertasks.modelos
+
+import java.util.Date //Documentacion https://developer.android.com/reference/kotlin/java/util/Date
+
 //Clase la cual representa eventos
 
 class Evento {
+    private var id_evento: Int = 0 //ID de la base de datos
+        get()= field
+        set(value) {field = value }
+    private var fecha: Date = Date()//Fecha de el evento
+        get() = field
+        set(value) {field = value}
+    private var nombre: String = "Nombre de evento" //nombre del evento
+        get() = field
+        set(value) {field = value}
+    private var descripcion: String? = "Descripcion de evento" //Descripcion del evento
+        get() = field
+        set(value) {field = value}
+    private var prioridad: Int? = 0 //Prioridad de el evento
+        get() = field
+        set(value) {field = value}
+    private var color: String? = null //Color de el evento, por ver si se implementara
+        get() = field
+        set(value) {field = value}
+
+    //Constructores
+    //Constructor completo
+    constructor(nombre: String, fecha: Date, descripcion: String, prioridad: Int, color: String){
+        this.nombre = nombre
+        this.fecha = fecha
+        this.descripcion = descripcion
+        this.prioridad = prioridad
+        this.color = color
+    }
+
+    //Constructor completo con fecha hecha con numeros (hora en militar)
+    constructor(nombre: String, dia: Int, mes: Int, anio: Int, hora: Int, minuto: Int,
+                descripcion: String, prioridad: Int, color: String){
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.prioridad = prioridad
+        this.color = color
+
+        //Crear fecha
+        this.fecha = Date(anio, mes, dia, hora, minuto)
+    }
+
+    //Constructor con solo cosas necesarias
+    constructor(nombre: String, fecha: Date){
+        this.nombre = nombre
+        this.fecha = fecha
+    }
+
+    //Constructor con cosas necesarias con horas hechas con numeros (hora en militar)
+    constructor(nombre: String, dia: Int, mes: Int, anio: Int, hora: Int, minuto: Int){
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.prioridad = prioridad
+        this.color = color
+
+        this.fecha = Date(anio, mes, dia, hora, minuto)
+    }
+
+
+    //Metodo para
+    override fun toString(): String {
+        return "Evento(id_evento=$id_evento, fecha=$fecha, nombre='$nombre', descripcion=$descripcion, prioridad=$prioridad, color=$color)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Evento
+
+        if (fecha != other.fecha) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return fecha.hashCode()
+    }
+
+    //Sets and getters
+
 }
