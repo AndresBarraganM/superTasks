b5f8df869498e2ad5cce9f010e746c624feb9620
salva <salvadorlaguna155@gmail.com>
2024-05-02 10:35:12 -0700

    Primera interfaz funcionando

--- app/src/main/java/com/example/supertasks/ventanas/ActivityCalendario.java
+++ app/src/main/java/com/example/supertasks/ventanas/ActivityCalendario.java
@@ -2,39 +2,39 @@ package com.example.supertasks.ventanas;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.supertasks.R;
 import java.util.Calendar;
 
-public class ActivityCalendario extends AppCompatActivity{
+public class ActivityCalendario extends AppCompatActivity {
+
     TextView tv;
 
-    // 26-04-2024
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
         tv = findViewById(R.id.calendario);
-        initCalendario(tv);
-    }
+        CalendarView calendarioView = findViewById(R.id.calendario);
 
-    public void initCalendario(View view) {
-        Calendar calendario = Calendar.getInstance();
-        int anio = calendario.get(Calendar.YEAR);
-        int mes = calendario.get(Calendar.MONTH);
-        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        // Obtener la fecha actual y mostrarla en el TextView
+        Calendar calendarioActual = Calendar.getInstance();
+        int anio = calendarioActual.get(Calendar.YEAR);
+        int mes = calendarioActual.get(Calendar.MONTH);
+        int dia = calendarioActual.get(Calendar.DAY_OF_MONTH);
+        tv.setText(dia + "/" + (mes + 1) + "/" + anio); // El mes se indexa desde 0, así que sumamos 1
 
-        DatePickerDialog dpd = new DatePickerDialog(ActivityCalendario.this, new DatePickerDialog.OnDateSetListener() {
+        // Establecer un Listener para el cambio de fecha en el CalendarView
+        calendarioView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String fecha = dayOfMonth + "/" + month + "/" + year;
-                tv.setText(fecha);
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // Actualizar el TextView con la fecha seleccionada
+                tv.setText(dayOfMonth + "/" + (month + 1) + "/" + year); // El mes se indexa desde 0, así que sumamos 1
             }
-        }, anio, mes, dia);
-        dpd.show();
+        });
     }
 }
-
