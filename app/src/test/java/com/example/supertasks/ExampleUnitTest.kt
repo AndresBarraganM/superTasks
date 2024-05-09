6f4e4d2d1c3e316332ce261219bbd4aaa1c60edb
AndresBarraganM <al22760549@ite.edu.mx>
2024-05-09 09:41:55 -0700

    test para eventos guardados, agregar y mostrar

--- app/src/test/java/com/example/supertasks/ExampleUnitTest.kt
+++ app/src/test/java/com/example/supertasks/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.supertasks
 
+import com.example.supertasks.modelos.Evento
+import com.example.supertasks.modelos.EventosGuardados
 import org.junit.Test
 
-import org.junit.Assert.*
+import java.util.Date
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,34 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun funciona_eventos_guardados() {
+        var listaEventos: EventosGuardados = EventosGuardados()
+
+        //Probar agregar objetos
+        var dat = Date(2024,5,18,15,30)
+        var ev = Evento("nombre1",fecha = dat, "Es el primer evento", 2, "rojo")
+        listaEventos.agregarEvento(ev)
+
+        dat = Date(2024,4,27,17,0,0)
+        ev = Evento("nombre2",fecha = dat, "Es el segundo evento", 2, "azul")
+        listaEventos.agregarEvento(ev)
+
+        dat = Date(2024,5,18,15,30)
+        ev = Evento("nombre3",fecha = dat, "Es el tercer evento", 2, "amarillo")
+        listaEventos.agregarEvento(ev)
+
+        dat = Date(2024,3,18,10,30)
+        ev = Evento("nombre4",fecha = dat, "Es el cuarto evento", 2, "amarillo")
+        listaEventos.agregarEvento(ev)
+
+        dat = Date(2024,5,10,18,27)
+        ev = Evento("nombre5",fecha = dat, "Es el cuarto evento", 2, "amarillo")
+        listaEventos.agregarEvento(ev)
+
+        //Ver datos
+        println(listaEventos.listaDeEventosFuturos())
+        println(listaEventos.listaDeEventosYaHechos())
+
+        //Eliminar datos
     }
 }
