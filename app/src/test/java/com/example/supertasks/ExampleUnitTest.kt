53ac218ccf76e21d9530599b69dcc99adc81ed1c
AndresBarraganM <al22760549@ite.edu.mx>
2024-05-14 16:01:51 -0700

    Prueva para cantidad de ventos en un plazo

--- app/src/test/java/com/example/supertasks/ExampleUnitTest.kt
+++ app/src/test/java/com/example/supertasks/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.supertasks
 import com.example.supertasks.modelos.Evento
 import com.example.supertasks.modelos.EventosGuardados
 import org.junit.Test
+import java.util.Calendar
 
 import java.util.Date
 
@@ -16,27 +17,38 @@ class ExampleUnitTest {
     fun funciona_eventos_guardados() {
         val listaEventos = EventosGuardados()
 
+
+        val ahora = Calendar.getInstance().time
+        println("Tiempo de ahora: "+ ahora)
+
         //Probar agregar objetos
-        var dat = Date(2024,5,18,15,30)
-        var ev = Evento("nombre1",fecha = dat, "Es el primer evento", 2, "rojo")
+        var dat = Date(124,3,9,18,53)
+        var ev = Evento("nombre1", dat, "Es el primer evento", 2, "rojo")
         listaEventos.agregarEvento(ev)
 
-        dat = Date(2024,4,27,17,0,0)
-        ev = Evento("nombre2",fecha = dat, "Es el segundo evento", 2, "azul")
+        dat = Date(124,3,20,17,0)
+        ev = Evento("nombre2", dat, "Es el segundo evento", 2, "azul")
         listaEventos.agregarEvento(ev)
 
-        dat = Date(2024,5,10,15,30)
-        ev = Evento("nombre3",fecha = dat, "Es el tercer evento", 2, "amarillo")
+        dat = Date(124,4,10,20,30)
+        ev = Evento("nombre3", dat, "Es el tercer evento", 2, "amarillo")
         listaEventos.agregarEvento(ev)
 
-        dat = Date(2024,3,18,10,30)
-        ev = Evento("nombre4",fecha = dat, "Es el cuarto evento", 2, "amarillo")
+        dat = Date(124,3,18,10,30)
+        ev = Evento("nombre4", dat, "Es el cuarto evento", 2, "amarillo")
         listaEventos.agregarEvento(ev)
 
-        dat = Date(2024,6,10,18,27)
-        ev = Evento("nombre5",fecha = dat, "Es el cuarto evento", 2, "amarillo")
+        dat = Date(124,4,15,18,27)
+        ev = Evento("nombre5", dat, "Es el quinto evento", 2, "amarillo")
         listaEventos.agregarEvento(ev)
 
+        dat = Date(124,4,26,18,27)
+        ev = Evento("nombre6", dat, "Es el sexto evento", 2, "amarillo")
+        listaEventos.agregarEvento(ev)
+
+        dat = Date(0,0,0,18,27)
+        ev = Evento("Evento a borrar", dat, "Es el quinto evento", 2, "amarillo")
+        listaEventos.agregarEvento(ev)
         //Ver datos
         println(listaEventos.listaDeEventosFuturos())
         println(listaEventos.listaDeEventosYaHechos())
@@ -44,5 +56,16 @@ class ExampleUnitTest {
         //Eliminar datos
         listaEventos.eliminarEvento(ev)
         println(listaEventos.listaDeEventosFuturos())
+
+        //Probar modificar evento
+        //TODO Falta hacer que agregar evento devuelva el evento con su id
+
+        //Ver metodo para cantidad de eventos
+        println("eventos por dia " +
+        listaEventos.cantidadEventosPendientes("dia"))
+        println("eventos por semana " +
+                listaEventos.cantidadEventosPendientes("semana"))
+        println("eventos por mes " +
+                listaEventos.cantidadEventosPendientes("mes"))
     }
 }
